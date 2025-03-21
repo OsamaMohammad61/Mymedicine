af8a81b2d4dca2c9aac374a648856cd996d92712
OsamaMohammad61 <osamamohammad343@gmail.com>
2025-03-22 02:01:03 +0300

    adding cart

--- Model/cart.cs
+++ Model/cart.cs
@@ -2,5 +2,12 @@
 {
     public class cart
     {
+        public int Id { get; set; }
+        public int? user_id { get; set; } = 0;
+        public double? totalAmount { get; set; } = 0;
+
+        public ICollection<product> product { get; set; }
     }
+    
+    
 }
